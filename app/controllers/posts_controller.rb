eb5430e53e488c8f74a906656fcb2abc7e223cf1
sreddy11 <sreddy1.sr@gmail.com>
2013-06-05 12:59:17 -0400

    added CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,18 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to(@post)
+    else
+       render(:new)
+    end
+  end
 end
 
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -27,4 +27,40 @@ describe PostsController do
       assigns[:post].should == post
     end
   end
+
+  describe "Get to #new" do
+     
+    before do
+      get :new
+    end
+
+    it { should respond_with(:success) }
+    it "assigns the post" do
+      assigns[:post].should_not == nil
+    end
+  end
+
+  describe "Post to #create" do
+    context "fields are valid" do
+      before do
+        post :create, :post => {:title => "new title", :content => "created content"}
+      end
+
+      it { should respond_with(:redirect) }
+      it { should redirect_to(post_path(assigns[:post].id)) }
+    end
+
+    context "fields are invalid" do
+      before do
+        post :create
+      end
+      it { should respond_with(:success) }
+      it "Has errors on the post" do
+         assigns[:post].errors.any?.should == true
+      end
+    end
+
+  end
+
+    
 end
