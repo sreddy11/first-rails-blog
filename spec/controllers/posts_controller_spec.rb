7717d9270a51fcd40b65261cf4e09fc9fb12714c
sreddy11 <sreddy1.sr@gmail.com>
2013-06-05 12:24:27 -0400

    added test for showing post and hyperlinks

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -14,4 +14,17 @@ describe PostsController do
       assigns[:posts].should == posts
     end
   end
+
+  describe "Get to #show" do
+    let!(:post) { FactoryGirl.create(:post) }
+
+    before do
+      get :show, :id => post.id
+    end
+
+    it { should respond_with(:success) }
+    it "assigns the post" do
+      assigns[:post].should == post
+    end
+  end
 end
