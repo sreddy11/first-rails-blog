fb9769f6e6a6d9059697e1c6ebe9001b53d2f711
sreddy11 <sreddy1.sr@gmail.com>
2013-06-05 14:46:07 -0400

    edit and create methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,5 +20,19 @@ class PostsController < ApplicationController
        render(:new)
     end
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      redirect_to(@post)
+    else
+      render(:edit)
+    end
+  end
+
 end
 
